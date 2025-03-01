585787ec86b746fc73aa4ad46cea500519590177
Juan Andres Alvarez <juanandres1029@gmail.com>
2025-03-01 12:31:07 -0500

    añade mejoras en templates

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { InfoService } from '../../shared/services/info.service';
+import { ProductServiceService } from '../../shared/services/product.service.service';
 
 @Component({
   selector: 'app-home',
@@ -12,8 +13,10 @@ import { InfoService } from '../../shared/services/info.service';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+public productService = inject(ProductServiceService)  
 infocomercio = inject(InfoService)
-
+//debo incluir el endpoint para obtener productos destacados
+//injectar el servicio de carrito
 
 
 
